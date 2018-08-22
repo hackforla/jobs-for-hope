3b1dbe837a42cc3aaac4cdab805fb706a3a33849
Stefanie Chin <chinsk0303@student.laccd.edu>
2018-08-22 13:54:06 -0700

    feat and test: add searchField function and test that values are being returned

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,13 @@
-alert("Yo")
+'use strict';
+
+
+/***Homepage: Searchfield***/
+function searchField() {
+
+
+	const inputSearch = document.querySelector('.search-input')
+	const inputLocation = document.querySelector('.location-input')
+
+	console.log(inputSearch.value)
+	console.log(inputLocation.value)
+}
