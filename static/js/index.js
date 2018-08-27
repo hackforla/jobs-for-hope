a19cebdc6bb8aaf1ab7e2222d249a4a7556aa48d
Stefanie Chin <chinsk0303@student.laccd.edu>
2018-08-27 16:19:26 -0700

    feat: add responsive navbar

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,16 @@
 'use strict';
 
+/****Responsive navbar****/
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+
 
 /***Homepage: Searchfield  TEMPLATE***/
 const url = '';
