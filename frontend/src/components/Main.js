e14654aac1c4564ca7b7ec96553982768469d162
Stefanie Chin <hi.stef.c@gmail.com>
2018-10-23 21:04:55 -0700

    move SearchBox into Main Component

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import SearchBox from './SearchBox';
 
-const Main = () => (
-  <h1>Main page</h1>
+const Main = ({ searchChange }) => (
+  <div>
+    <h1>Main page</h1>
+    <SearchBox searchChange={searchChange} />
+  </div>
 );
 
 export default Main;
