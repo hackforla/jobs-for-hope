19440b40a34fdaad6c9281434af8fb82082b4120
Stefanie Chin <hi.stef.c@gmail.com>
2018-11-10 15:49:06 -0800

    chore: clean up filters

--- frontend/src/components/SideFilter.js
+++ frontend/src/components/SideFilter.js
@@ -18,49 +18,26 @@ const SideFilter = ({checkedEmploymentType, onSetEmploymentTypeFT, onSetEmployme
                 <p>Employment Type</p>
                 <div className="filter-options">
                     <input name="full-time" type="checkbox" onChange={event => onSetEmploymentTypeFT(event)} />
-                    <p> Full Time (14)</p> <br />
+                    <p>Full Time</p> <br />
                 </div>
                 <div className="filter-options">
                     <input name="part-time" type="checkbox" onChange={event => onSetEmploymentTypePT(event)} />
-                    <p> Part Time (14)</p> <br />
-                </div>
-            </div>
-            <div className="department-type filter-criteria">
-                <p> Department Type</p>
-                <div className="filter-options">
-                    <input name="housing" type="checkbox" />
-                    <p> Housing (14)</p> <br />
-                </div>
-                <div className="filter-options">
-                    <input name="management" type="checkbox" />
-                    <p> Management (14)</p> <br />
-                </div>
-                <div className="filter-options">
-                    <input name="operations" type="checkbox" />
-                    <p> Operations (14)</p> <br />
-                </div>
-                <div className="filter-options">
-                    <input name="accounting" type="checkbox" />
-                    <p> Accounting (14)</p> <br />
-                </div>
-                <div className="filter-options">
-                    <input name="human-resources" type="checkbox" />
-                    <p> Human Resources (14)</p> <br />
+                    <p>Part Time</p> <br />
                 </div>
             </div>
             <div className="experience-level filter-criteria">
                 <p> Experience Level</p>
                 <div className="filter-options">
                     <input name="entry-level" type="checkbox" />
-                    <p> Entry Level (14)</p> <br />
+                    <p>Entry Level</p> <br />
                 </div>
                 <div className="filter-options">
                     <input name="mid-level" type="checkbox" />
-                    <p> Mid Level (14)</p> <br />
+                    <p>Mid Level</p> <br />
                 </div>
                 <div className="filter-options">
                     <input name="senior-level" type="checkbox" />
-                    <p> Senior Level (14)</p> <br />
+                    <p>Senior Level</p> <br />
                 </div>
             </div>
         </aside>
