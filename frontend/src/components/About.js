2ea96d607cb56f9ddd3f1abda8b36e461983984c
Stefanie Chin <hi.stef.c@gmail.com>
2018-11-24 14:27:55 -0800

    added: 'Coming soon message'

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -7,6 +7,7 @@ const About = (props) => {
   return(
     <div>
       <Banner titleUpper={'A Paycheck With'} titleLower={'A Purpose'} imageName='homeless_sitting' />
+      <h2 style={{margin: '25px', fontSize: '2em'}}>Coming Soon</h2>
     </div>
   );
 }
