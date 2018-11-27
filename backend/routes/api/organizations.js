fc90e8c7c41f55d0cf96697d99d13bb6a543c004
Fang Yi Liu <fangyiliu@gmail.com>
2018-11-27 11:20:10 -0800

    initial node backend
    Node backend with api for existing frontend data needs. Frontend is
    modified to call the backend.

---
+++ backend/routes/api/organizations.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const fetch = require("node-fetch");
+
+// @route   GET api/orgs
+// @desc    Get all orgs
+// @access  Public
+router.get('/', (req, res) => {
+  const url = "https://spreadsheets.google.com/feeds/list/16npDyyzNjgZ2h5uZmRNs2T2RRUCJtHB_1eHpmxUr1SI/3/public/values?alt=json"
+  fetch(url)
+    .then(data => data.json())
+    .then(orgs => res.json(orgs.feed.entry))
+    .catch(err => res.status('404').json({ "error": "Error loading orgs" }));
+});
+
+module.exports = router;
