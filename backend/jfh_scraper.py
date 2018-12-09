2f50dd33f5ffd816b7c6787b55cd8fa94d96b3e5
George Su <georgesu1074@berkeley.edu>
2018-12-08 22:34:49 -0800

    Finished scraping The Village Family Services. Retrieved every field except salary.

--- backend/jfh_scraper.py
+++ backend/jfh_scraper.py
@@ -755,8 +755,27 @@ reset_vars()
 # The Village Family Services
 
 organization = "The Village Family Services"
+soup = get_soup('http://new.thevillagefs.org/jobs/type/default/')
 
-## SCRAPING CODE
+job_grid = soup.find('div',{'class':'wpjb-job-list'})
+
+for job_div in job_grid.find_all('div',{'class':'wpjb-col-main'}):
+    major_line = job_div.find('div',{'class':'wpjb-line-major'})
+    job_title = major_line.a.text
+    info_link = major_line.a['href']
+    full_or_part = major_line.find('span',{'class':'wpjb-sub-title'}).text.strip()
+    minor_line = job_div.find('div',{'class':'wpjb-line-minor'})
+    job_location = minor_line.find('span',{'class':'wpjb-job_location'}).text.strip()
+    date = minor_line.find('span',{'class':'wpjb-job_created_at'}).text.strip().split(', ')
+    month = month_string_to_num(date[0])
+    day = int(date[1])
+    if month <= datetime.now().month:
+        year = datetime.now().year
+    else:
+        year = datetime.now().year-1
+    job_post_date = datetime(year, month, day)
+    update_db()
+    reset_vars()
 
 reset_vars()
 
