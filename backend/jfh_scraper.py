90392d5338e580fb5ddfd020d61d08f3aec0640f
George Su <georgesu1074@berkeley.edu>
2018-12-08 16:26:38 -0800

    Finished SHARE! the Self Help and Recovery Exchange scraper. Retrieved job title, link, and job location.

--- backend/jfh_scraper.py
+++ backend/jfh_scraper.py
@@ -622,8 +622,13 @@ reset_vars()
 # SHARE! the Self Help And Recovery Exchange
 
 organization = "SHARE! the Self Help And Recovery Exchange"
+soup = get_soup("https://shareselfhelp.org/programs-share-the-self-help-and-recovery-exchange/share-jobs-share-self-help-recovery-exchange/")
 
-## SCRAPING CODE
+for html_element in soup.find_all('h4'):
+    job_title = html_element.a.text
+    info_link = html_element.a['href']
+    job_location = html_element.span.text.split(']')[1]
+    update_db()
 
 reset_vars()
 
