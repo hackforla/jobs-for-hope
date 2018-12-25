ff5f341f1402ff07c841a7a34800caf07b674aac
George Su <georgesu1074@berkeley.edu>
2018-12-25 01:27:31 -0800

    Finished PATH scraper. Some more info possible if each individual job listing is scraped, but job listings are too poorly standardized to scrape reliably.

--- backend/jfh_scraper.py
+++ backend/jfh_scraper.py
@@ -897,10 +897,29 @@ reset_vars()
 
 
 # People Assisting the Homeless (PATH)
-
 organization = "People Assisting the Homeless"
+soup = get_soup('https://path.catsone.com/careers')
 
-## SCRAPING CODE
+jobs_table = soup.find('div',{'class':'JobGrid-etzr7g-4'})
+
+for job_entry in jobs_table.find_all('a'):
+    info_link = 'https://path.catsone.com' + job_entry['href']
+    job_row = job_entry.find('div',{'class':'row'})
+    job_divs = job_row.find_all('div')
+    job_title = job_divs[0].text.strip()
+    job_location = clean_location(job_divs[2].text.strip())
+    job_zip_code = city_to_zip(job_location)
+    update_db(organization)
+    reset_vars()
+    # Possible to get more info by scraping each job link, but the listings are extremely poorly written/standardized; scraper below works for most of the listings, but a few poorly written listings break the scraper
+    # job_soup = get_soup(info_link)
+    # job_description = job_soup.find('div',{'class':'Job__StyledDescription-s1h17u0t-0'})
+    # if '\n' in job_description.find_all('strong')[0].text:
+    #     full_or_part = job_description.find_all('strong')[0].text.split('\n')[1].strip()
+    #     salary = job_description.find_all('strong')[0].text.split('\n')[2].strip().split(': ')[1]
+    # else:
+    #     full_or_part = job_description.find_all('strong')[1].text.strip()
+    #     salary = job_description.find_all('strong')[2].text.split('\n')[0].split(':')[1].strip()
 
 reset_vars()
 
