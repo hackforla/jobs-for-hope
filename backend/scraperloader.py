982b0a29475f03a899961590cb1614f775eabb95
Fang Yi Liu <fangyiliu@gmail.com>
2019-01-16 13:42:55 -0800

    Remove initial debug prints for scraperloader

--- backend/scraperloader.py
+++ backend/scraperloader.py
@@ -10,20 +10,20 @@ MainModule = '__init__'
 def getScrapers():
     scrapers = []
     possible_scrapers = os.listdir(ScraperFolder)
-    print(possible_scrapers)
+    #print(possible_scrapers)
     for i in possible_scrapers:
         location = os.path.join(ScraperFolder, i)
-        print(location)
-        print( os.path.splitext(location)[-1].lower() == '.py')
+        #print(location)
+        #print( os.path.splitext(location)[-1].lower() == '.py')
         if os.path.isdir(location) or not os.path.splitext(location)[-1].lower() == '.py':
             continue
         scrapers.append({'name': i, 'path': location})
-    print(scrapers)
+    #print(scrapers)
     return scrapers
 
 def loadScraper(scraper):
-    print('Loading module %s' % scraper['path'])
-    print('Loading module %s' % scraper['name'])
+    #print('Loading module %s' % scraper['path'])
+    #print('Loading module %s' % scraper['name'])
 
     searchPath, file = os.path.split(scraper['path'])
     moduleName, ext = os.path.splitext(file)
