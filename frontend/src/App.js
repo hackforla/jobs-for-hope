58ae7fcbf4c709f34237ea53fdbeda3a09132748
John Darragh <darragh@entrotech.net>
2019-03-12 15:45:43 -0700

    remove commented out code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,51 +10,7 @@ import Footer from "./components/Footer";
 
 import * as jobService from "./services/job-service";
 import * as organizationService from "./services/organization-service";
-
-// import {
-//   setSearchField,
-//   setSearchZip,
-//   fetchJobs,
-//   fetchOrganizations,
-//   setEmploymentTypeFT,
-//   setEmploymentTypePT,
-//   setDistance
-// } from "./store/actions";
-
 import "./App.scss";
-
-// const mapStateToProps = state => ({
-//   searchField: state.searchJobListing.searchField,
-//   zipcode: state.searchJobListing.zipcode,
-
-//   isPending: state.requestJobs.isPending,
-//   jobData: state.requestJobs.jobData,
-//   isError: state.requestJobs.isError,
-
-//   orgsArePending: state.requestOrgs.orgsArePending,
-//   organizationData: state.requestOrgs.organizationData,
-//   orgsGetDataError: state.requestOrgs.orgsGetDataError,
-
-//   employmentTypeFT: state.changeEmploymentType.FT,
-//   employmentTypePT: state.changeEmploymentType.PT,
-
-//   distance: state.changeDistance.distance
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   onSearchChange: event => dispatch(setSearchField(event.target.value)),
-//   onZipSearchChange: event => dispatch(setSearchZip(event.target.value)),
-
-//   onfetchJobs: () => dispatch(fetchJobs()),
-//   onfetchOrgs: () => dispatch(fetchOrganizations()),
-
-//   onSetEmploymentTypeFT: event =>
-//     dispatch(setEmploymentTypeFT(event.target.checked)),
-//   onSetEmploymentTypePT: event =>
-//     dispatch(setEmploymentTypePT(event.target.checked)),
-
-//   onSetDistance: event => dispatch(setDistance(event.target.value))
-// });
 
 class App extends Component {
   state = {
@@ -98,58 +54,6 @@ class App extends Component {
       });
   }
 
-  // calcDistance = () => {
-  //   const { zipcode, jobData, distance } = this.props;
-  //   let populateFilteredJobs = jobData;
-
-  //   //this needs jobData again
-  //   if (distance === "10") {
-  //     //if jobTitle...gotta filter by job title first!
-  //     //use zipcodes to calculate distance of each job posting: zipcode
-  //     populateFilteredJobs = populateFilteredJobs
-  //       .map(job => {
-  //         job.distance = dist(zipcode, job.zipcode);
-  //         return job;
-  //       })
-  //       .filter(jobs => jobs.distance <= 10)
-  //       .filter(job =>
-  //         job.title.toLowerCase().includes(this.props.searchField.toLowerCase())
-  //       );
-
-  //     this.setState({
-  //       filteredJobs: populateFilteredJobs
-  //     });
-  //   } else if (distance === "25") {
-  //     populateFilteredJobs = populateFilteredJobs
-  //       .map(job => {
-  //         job.distance = dist(zipcode, job.zipcode);
-  //         return job;
-  //       })
-  //       .filter(jobs => jobs.distance <= 25)
-  //       .filter(job =>
-  //         job.title.toLowerCase().includes(this.props.searchField.toLowerCase())
-  //       );
-
-  //     this.setState({
-  //       filteredJobs: populateFilteredJobs
-  //     });
-  //   } else if (distance === "1000") {
-  //     populateFilteredJobs = populateFilteredJobs
-  //       .map(job => {
-  //         job.distance = dist(zipcode, job.zipcode);
-  //         return job;
-  //       })
-  //       .filter(jobs => jobs.distance <= 1000)
-  //       .filter(job =>
-  //         job.title.toLowerCase().includes(this.props.searchField.toLowerCase())
-  //       );
-
-  //     this.setState({
-  //       filteredJobs: populateFilteredJobs
-  //     });
-  //   }
-  // };
-
   render() {
     const { isPending, organizations, jobs } = this.state;
 
@@ -161,20 +65,7 @@ class App extends Component {
           <header className="header">
             <Navbar />
           </header>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              // <Jobs
-              //   {...this.props}
-              //   filteredJobs={this.state.filteredJobs}
-              //   userJobTitle={this.userJobTitle}
-              //   calcDistance={this.calcDistance}
-              //   filterByEmploymentType={this.filterByEmploymentType}
-              // />
-              <Jobs jobs={jobs} />
-            )}
-          />
+          <Route exact path="/" render={() => <Jobs jobs={jobs} />} />
           <Route
             path="/organizations"
             render={() => <Organizations organizations={organizations} />}
@@ -189,8 +80,3 @@ class App extends Component {
 }
 
 export default App;
-
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(App);
