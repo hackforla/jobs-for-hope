86604b255503ba2c2124c9c0d5bcc479f14463a0
Fang Yi Liu <fangyiliu@gmail.com>
2019-03-19 16:19:12 -0700

    Import from datetime in parser globals

--- backend/globals.py
+++ backend/globals.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from uszipcode import SearchEngine as search
+from datetime import datetime, timedelta
 
 # GLOBALS
 
