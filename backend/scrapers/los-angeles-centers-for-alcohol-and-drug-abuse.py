373af16147dbbf6d8ea745d4dc72525195e4da02
Fang Yi Liu <fangyiliu@gmail.com>
2019-03-19 16:16:37 -0700

    Add Los Angeles Centers for Alcohol and Drug Abuse scraper plugin

---
+++ backend/scrapers/los-angeles-centers-for-alcohol-and-drug-abuse.py
@@ -0,0 +1,15 @@
+import globals
+from globals import get_soup, update_db
+
+# Los Angeles Centers For Alcohol and Drug Abuse
+
+organization = "Los Angeles Centers For Alcohol and Drug Abuse"
+url = 'http://www.lacada.com/2018/career-opportunities/'
+
+def run(url):
+    soup = get_javascript_soup(url)
+    jobs_list = soup.select('div.wpb_wrapper > p > a')
+    for job_entry in jobs_list:
+        globals.job_title = job_entry.text.strip()
+        globals.info_link = job_entry['href']
+        update_db(organization)
