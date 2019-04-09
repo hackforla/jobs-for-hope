8bdcc2e74fceea6bba5c6cc35f78aed3328d5874
Fang Yi Liu <fangyiliu@gmail.com>
2019-04-09 12:50:50 -0700

    Gather sqlite connection code into a function to prepare for postgres

--- backend/scraper_runner.py
+++ backend/scraper_runner.py
@@ -9,33 +9,40 @@ from os.path import basename
 
 pdf_message = 'See PDF document.'
 
-# SQL CONNECTION
+def connect_sqlite():
+    # SQL CONNECTION
+    globals.db = sqlite3.connect("jobs_for_hope.db")
+    globals.c = globals.db.cursor()
 
-globals.db = sqlite3.connect("jobs_for_hope.db")
-globals.c = globals.db.cursor()
-
-# Create SQL schema if necessary
-globals.create_table_jobs()
-
-globals.reset_vars()
-
-# set the active scraper if one is passed in
-if len(sys.argv) - 1 == 1:
-    globals.active_scrapers = [basename(sys.argv[1])]
-
-# load and run scrapers
-for i in scraperloader.getScrapers():
-    try:
-        # filter to run target scrapers
-        if len(globals.active_scrapers) > 0 and not i['name'] in globals.active_scrapers:
-            continue
-        scraper = scraperloader.loadScraper(i)
-        organization = scraper.organization
-        print organization
-        globals.delete_jobs_by_organization(organization)
-        scraper.run(scraper.url)
-    except Exception:
-        traceback.print_exc()
-        print 'Scraper failed:', organization
+    # Create SQL schema if necessary
+    globals.create_table_jobs()
 
     globals.reset_vars()
+
+    # set the active scraper if one is passed in
+    if len(sys.argv) - 1 == 1:
+        globals.active_scrapers = [basename(sys.argv[1])]
+
+    # load and run scrapers
+    for i in scraperloader.getScrapers():
+        try:
+            # filter to run only active scrapers
+            if len(globals.active_scrapers) > 0 and not i['name'] in globals.active_scrapers:
+                continue
+            scraper = scraperloader.loadScraper(i)
+            organization = scraper.organization
+            print organization
+            globals.delete_jobs_by_organization(organization)
+            scraper.run(scraper.url)
+        except Exception:
+            traceback.print_exc()
+            print 'Scraper failed:', organization
+
+        globals.reset_vars()
+
+
+    try:
+
+
+if __name__ == '__main__':
+    connect_sqlite()
