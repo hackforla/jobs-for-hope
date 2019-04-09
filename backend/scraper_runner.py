df267f56a6af9c893f39b5c5103e79957e447f3d
Fang Yi Liu <fangyiliu@gmail.com>
2019-04-09 12:56:54 -0700

    Add test code for postgres

--- backend/scraper_runner.py
+++ backend/scraper_runner.py
@@ -2,8 +2,10 @@ import sys
 import traceback
 import scraperloader
 import sqlite3
+import psycopg2
 import globals
 from os.path import basename
+from config import config
 
 # CONSTANTS
 
@@ -41,8 +43,36 @@ def connect_sqlite():
         globals.reset_vars()
 
 
+def connect_pg():
+    """ Connect to the PostgreSQL database server """
+    conn = None
     try:
+        # read connection parameters
+        params = config()
 
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        globals.cur = conn.cursor()
+
+        # execute a statement
+        print('PostgreSQL database version:')
+        globals.cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = globals.cur.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        globals.cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
 
 if __name__ == '__main__':
-    connect_sqlite()
+    connect_pg()
