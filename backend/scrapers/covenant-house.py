8ba9386a3df4b49bab018c204327769572ad84be
Fang Yi Liu <fangyiliu@gmail.com>
2019-05-07 22:38:37 -0700

    Convert 7 scrapers to plugins
    Coalition for Responsible Community Development
    Covenant House
    Exodus Recovery, Inc.
    Hathaway-Sycamores Child and Family Services
    Illumination Foundation
    Jewish Family Service of Los Angeles
    Los Angeles LGBT Center

---
+++ backend/scrapers/covenant-house.py
@@ -0,0 +1,21 @@
+import globals
+from globals import get_soup, update_db
+
+# Covenant House
+
+organization = "Covenant House"
+url = 'https://covca.hrmdirect.com/employment/job-openings.php?search=true&nohd=&dept=-1&city=-1&state=-1'
+
+def run(url):
+    soup = get_soup(url)
+    jobs_list = soup.find_all("tr", {"class": "reqitem"})
+
+    for job_entry in jobs_list:
+        for child in job_entry.find_all("td", {"class": "posTitle"}):
+            for child2 in child.find_all("a"):
+                globals.job_title = child.text
+                globals.info_link = "https://covca.hrmdirect.com/" + child2.get('href')
+        for child in job_entry.find_all("td", {"class": "cities"}):
+            job_location = child.text
+        if(job_location == "Los Angeles"):
+            update_db(organization)
