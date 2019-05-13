2fb3acc3477bbc07319a7dabe31a213f324fbcb0
Fang Yi Liu <fangyiliu@gmail.com>
2019-05-13 14:31:43 -0700

    Fix scrapers inserting blank job_title fields
    Some sites will leave blank links so we ignore those.

--- backend/scrapers/la-family-housing-corporation.py
+++ backend/scrapers/la-family-housing-corporation.py
@@ -12,6 +12,11 @@ def run(url):
     jobs_list = jobs_div.find_all('p')
 
     for job_entry in jobs_list[4:len(jobs_list)-3]:
-        globals.job_title = job_entry.a.text.strip()
-        globals.info_link = 'https://lafh.org' + job_entry.a['href']
-        update_db(organization)
+        # some job line have multiple job links but only one contains anchor
+        # text
+        job_links = job_entry.find_all('a')
+        for job_link in job_links:
+            if len(job_link.text.strip()):
+                globals.job_title = job_link.text.strip()
+                globals.info_link = 'https://lafh.org' + job_link['href']
+                update_db(organization)
