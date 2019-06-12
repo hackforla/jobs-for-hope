547bd4669faa3d3c81028072aed7c50bbe45385e
Fang Yi Liu <fangyiliu@gmail.com>
2019-06-12 15:21:37 -0700

    Stub out employer account functionality

--- client/src/components/controls/EmployerControls.js
+++ client/src/components/controls/EmployerControls.js
@@ -9,9 +9,11 @@ const EmployerControls = props => {
     <div className="employer-main">
       <div className="jobs-container">
         <h2>Jobs:</h2>
+        create jobs, edit jobs, delete jobs
       </div>
       <div className="org-info-container">
         <h2>Organization Info:</h2>
+        edit org info, associate another org
       </div>
     </div>
   );
