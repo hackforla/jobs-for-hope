f867bf48d0e1b400e7710d219d107f0c8d2257c6
Fang Yi Liu <fangyiliu@gmail.com>
2019-06-28 12:15:51 -0700

    Use SSL for DB connection

--- services/postgres-pool.js
+++ services/postgres-pool.js
@@ -5,7 +5,8 @@ const pool = new Pool({
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT
+  port: process.env.POSTGRES_PORT,
+  ssl: true
 });
 
 module.exports = {
