91024af89964c9d5dc254d68870b3210f5af2592
Fang Yi Liu <fangyiliu@gmail.com>
2019-08-14 16:59:19 -0700

    Fix Clare Foundation scraper
    Added a delay for page load. The site may have an anti-bot mechanism
    that disconnects the scraper when it connects. But it scrapes after
    having visited the page using a web browser.

--- backend/scrapers/the_clare_foundation_inc.py
+++ backend/scrapers/the_clare_foundation_inc.py
@@ -1,6 +1,7 @@
 import globals
-from globals import get_soup, job_insert
+from globals import get_javascript_soup_delayed, job_insert
 from job import Job
+
 # The Clare Foundation, Inc.
 organization = "Clare Foundation, Inc."
 url = 'http://clarefoundation.org/careers/'
@@ -8,13 +9,13 @@ organization_id = 52
 
 
 def run(url):
-    soup = get_soup(url)
+    soup = get_javascript_soup_delayed(url, 'div#content')
 
-    listings_container = soup.find('ul', {'class': 'display-posts-listing'})
+    listings_container = soup.find('div', {'id': 'content'})
     job_class = Job(organization, "")
     job_class.organization_id = organization_id
     insert_count = 0
-    for listing in listings_container.find_all('li'):
+    for listing in listings_container.find_all('p'):
         job_class.title = listing.text
         job_class.info_link = listing.a['href']
         insert_count += job_insert(job_class)
