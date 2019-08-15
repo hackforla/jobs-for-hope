fc6b9f657c71b44531d5ad6c5ad7613dddb11e82
Fang Yi Liu <fangyiliu@gmail.com>
2019-08-14 17:17:05 -0700

    Fix PATH scraper using html tag structure
    The site has implemented anti-scraping measures by frequently changing
    the css class names. I'm trying to see if parsing it by tag struccture
    will help.

--- backend/scrapers/people_assisting_the_homeless.py
+++ backend/scrapers/people_assisting_the_homeless.py
@@ -10,11 +10,11 @@ organization_id= 40
 def run(url):
     soup = get_soup(url)
 
-    jobs_list = soup.select('div[class="sc-gGBfsJ gqaxtq"]')[0]
+    jobs_list = soup.select('body > div > div > div > div > div > div > div > div:nth-of-type(3) > div > div > div > div > div > div:nth-of-type(2) > a')
     job_class= Job(organization, "")
     job_class.organization_id= organization_id
     insert_count= 0
-    for job_entry in jobs_list.find_all('a'):
+    for job_entry in jobs_list:
         job_class.info_link = 'https://path.catsone.com' + job_entry['href']
         job_row = job_entry.find('div', {'class': 'row'})
         job_divs = job_row.find_all('div')
